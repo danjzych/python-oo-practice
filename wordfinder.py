68aa039aae63462a66a4f57b81681aa6a97fa8a1
ross-brown <rbrown_98@yahoo.com>
2023-08-22 18:55:05 -0500

    refactor and add tests

--- wordfinder.py
+++ wordfinder.py
@@ -1,28 +1,31 @@
 from random import choice
 
+
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a dictionary.
+
+    >>> test = WordFinder("test.txt")
+    8 words read
+
+    >>> test.random() in ['mango', 'banana', 'strawberry', '', '# this is a comment', '# so is this']
+    True
+
+    """
 
     def __init__(self, filepath):
         """Reads file and creates list of words."""
-        # open file here
-        #self.filepath = filepath
-        self.list_of_words = []
-        self.read_file() # this returns a list
-        #close file
+        file = open(filepath)
+        self.list_of_words = self.read_file(file)
+        file.close()
+
+        print(f"{len(self.list_of_words)} words read")
 
     def __repr__(self):
         return f"<WordFinder filepath={self.filepath}>"
 
-    #TODO::make this the file that is extended, not the random function
-    def read_file(self):
-        """Reads file line by line and appends to list of words."""
-        file = open(self.filepath)
-        for line in file:
-            self.list_of_words.append(line[:-1])
-        file.close()
-
-        print(f"{len(self.list_of_words)} words read")
+    def read_file(self, file):
+        """Reads file line by line and returns list of words."""
+        return [line[:-1] for line in file]
 
     def random(self):
         """Returns a random word from the list of words."""
@@ -30,18 +33,19 @@ class WordFinder:
 
 
 class SpecialWordFinder(WordFinder):
-    '''SpecialWordFinder - find words that aren\'t spaces or comments'''
+    '''SpecialWordFinder - find words that aren\'t spaces or comments
 
-    def __init__(self, filepath):
-        '''Creates an instance of SpecialWordFinder using filepath'''
-        super().__init__(filepath)
+    >>> test = SpecialWordFinder("test.txt")
+    3 words read
+
+    >>> test.random() in ['mango', 'banana', 'strawberry']
+    True
+
+    '''
 
     def __repr__(self):
         return f'SpecialWordFinder filepath={self.filepath}'
 
-    def random(self):
+    def read_file(self, file):
         '''Returns a random line that is not a comment or blank'''
-        valid_words = [
-            line for line in self.list_of_words if not line.startswith('#') and line
-            ]
-        return choice(valid_words)
+        return [line for line in super().read_file(file) if not line.startswith('#') and line != '']
