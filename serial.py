6dcdcc76d3eca3b928b6245e0888c6ad0c79bf69
Daniel Zych <danjzych@gmail.com>
2023-08-22 17:17:59 -0500

    Completed part one

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        '''Create serial generator starting at self'''
+        self.start = start
+        self.next = start
+
+    def __repr__(self):
+        return f'instance of SerialGenerator starting at {self.start}'
+
+    def generate(self):
+        '''Increments self.next by one and return it'''
+        currentValue = self.next
+        self.next += 1
+        return currentValue
+
+    def reset(self):
+        '''Resets self.next to initial start value'''
+        self.next = self.start
