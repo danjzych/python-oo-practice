bb66fd7cf7dbbd03421c95ecd82834116ee9aeab
ross-brown <rbrown_98@yahoo.com>
2023-08-22 17:38:18 -0500

    finish basic random word class

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,21 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.list_of_words = []
+        self.read_file()
+
+    def read_file(self):
+        file = open(self.filepath)
+        for line in file:
+            self.list_of_words.append(line[:-1])
+        file.close()
+
+        print(f"{len(self.list_of_words)} words read")
+
+    def random(self):
+        return choice(self.list_of_words)
+
