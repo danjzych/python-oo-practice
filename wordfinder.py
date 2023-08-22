979bd4e7166194c13d136815b9f9c531e476d861
ross-brown <rbrown_98@yahoo.com>
2023-08-22 17:41:07 -0500

    add docstrings and repr method

--- wordfinder.py
+++ wordfinder.py
@@ -4,11 +4,16 @@ class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, filepath):
+        """Reads file and creates list of words."""
         self.filepath = filepath
         self.list_of_words = []
         self.read_file()
 
+    def __repr__(self):
+        return f"<WordFinder filepath={self.filepath}>"
+
     def read_file(self):
+        """Reads file line by line and appends to list of words."""
         file = open(self.filepath)
         for line in file:
             self.list_of_words.append(line[:-1])
@@ -17,5 +22,5 @@ class WordFinder:
         print(f"{len(self.list_of_words)} words read")
 
     def random(self):
+        """Returns a random word from the list of words."""
         return choice(self.list_of_words)
-
